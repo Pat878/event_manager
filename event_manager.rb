a9c0263d21c7bfd4e46cb1ee6520b558c49f2aea
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-04-16 08:51:14 -0400

    Require ERB, create ERB template file, and simplify legislators_by_zipcode method

--- event_manager.rb
+++ event_manager.rb
@@ -1,6 +1,6 @@
 require "csv"
 require 'sunlight/congress'
-template_letter = File.read "form_letter.html"
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -9,18 +9,15 @@ def clean_zipcode(zipcode)
 end
 
 def legislators_by_zipcode(zipcode)
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
-
-  legislator_names.join(", ")
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
 puts "EventManager initialized."
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents.each do |row|
   name = row[:first_name]
@@ -29,5 +26,6 @@ contents.each do |row|
 
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+  puts form_letter
 end
